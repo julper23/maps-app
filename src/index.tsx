d76a4d3fdbb076b0f9556d4e49f4a6ec7d48e8b5
Julen Perez Coca <julper23@gmail.com>
2026-06-14 19:45:07 +0200

    Mi primer commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+/* eslint import/no-webpack-loader-syntax: off */
 
-const root = ReactDOM.createRoot(
+import React from 'react';
+import { MapsApp } from './MapsApp';
+import { createRoot } from 'react-dom/client';
+
+import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+ 
+mapboxgl.accessToken = 'pk.eyJ1Ijoia2xlcml0aCIsImEiOiJja3hramV2OWIwbjEwMzFwYzJlZWl6N2g5In0.iKXPpYvo7UPRiiZ-x_lCrw';
+
+
+if ( !navigator.geolocation ) {
+  alert( 'Tu navegador no tiene opción de Geolocation' );
+  throw new Error('Tu navegador no tiene opción de Geolocation');
+}
+
+
+// @ts-ignore
+const root = createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <MapsApp />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
